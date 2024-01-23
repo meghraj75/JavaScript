afdd1897ba258da412982c37f893341e2ca3e1f7
meghraj75 <mnarkhede.02@gmail.com>
2024-01-23 18:27:04 +0530

    javascipt folder added

---
+++ string/method.js
@@ -0,0 +1,12 @@
+let user="        megharaj";
+console.log(user.trim());
+let user1='       megharaj';
+console.log(user1);
+// touppercase and lowercase
+ console.log(`the lower case,${user.toLowerCase()}`);
+//  we can declare multiple method in one using , operator
+ console.log(user.trim(),user.toUpperCase());
+ //indexof return first occurence
+ let user2="ilovecoding";
+ let user3=user2.indexOf("love");
+  console.log("the given substring first occurence of love is 1:",user3);
---
+++ variable.js
@@ -0,0 +1,19 @@
+let a=10;
+let b=20;
+console.log(typeof(a)); // it willl return which type of data passed to this variable
+
+let z=true;
+let w="string value";
+let u=null;
+let v=undefined;
+console.log(z);
+console.log(w);
+console.log(u);
+console.log(v);
+v=90;   //after we definde variable v is undefind and we simply defined there value
+console.log(v);
+
+
+
+// NAN not a number
+    
